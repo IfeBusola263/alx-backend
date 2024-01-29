0ba70f5d436cb2272056751f104a5cedbb4a15dd
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-29 18:25:01 +0000

    corrected the keyname for total pages

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -103,4 +103,4 @@ class Server:
 
         return {'page_size': page_size, 'page': page, 'data': data,
                 'next_page': next_page, 'prev_page': prev_page,
-                'total_page': total_page}
+                'total_pages': total_page}
