89c9c5dc69916babc01152d3e57985d325004325
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-31 14:41:45 +0000

    Implemented the least frequently used algorithm

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -29,27 +29,29 @@ class LFUCache(BaseCaching):
         """
 
         if key is not None and item is not None:
-            # check if the key is being tracked
-            # everytime a key is used, it must be popped and appended
-            # to the LFUCache.tk so the least used would be at the top
-            # of the list
-            LFUCache.tk.append(
-                LFUCache.tk.pop(LFUCache.tk.index(key)
-                                ) if key in LFUCache.tk else key)
-            self.freq[key] = self.freq.get(key, 0) + 1
 
+            # insert the data into the cache
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                if self.freq.get(LFUCache.tk[0]) > 1:
-                    to_delete = LFUCache.tk.pop(-1)
-                    del self.cache_data[to_delete]
-                    del self.freq[to_delete]
-                else:
-                    to_delete = LFUCache.tk.pop(0)
-                    del self.freq[to_delete]
 
-                # remove it from the tracker and print it
-                print(f'DISCARD: {to_delete}')
+            # confirm the cache is not overloaded
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+
+                # sort the tracker to have least frequently used at the
+                # top of the list
+                lfu_order = sorted(self.freq.items(), key=lambda x: x[1])
+                lfu_key = lfu_order[0][0]
+                # print(self.freq)
+                # print(lfu_order)
+
+                # delete the least frequently used data in the cache
+                del self.cache_data[lfu_key]
+                print(f'DISCARD: {lfu_key}')
+
+                # delete it from the tracker as well
+                del self.freq[lfu_key]
+
+            # Update the tracker with recent addition
+            self.freq[key] = self.freq.get(key, 0) + 1
 
     def get(self, key):
         """
@@ -57,11 +59,9 @@ class LFUCache(BaseCaching):
         If the key is not in the cached data, None is returned.
         """
         if key is not None:
-            if key in LFUCache.tk:
-                LFUCache.tk.append(LFUCache.tk.pop(LFUCache.tk.index(key)))
-
-            if key in self.freq:
-                self.freq[key] = self.freq.get(key, 0) + 1
+            if key in self.cache_data:
+                # tracking data from here too
+                self.freq[key] = self.freq.get(key) + 1
 
             # The get method of dictionaries return None
             # if the key does not exists. But if it does, the
