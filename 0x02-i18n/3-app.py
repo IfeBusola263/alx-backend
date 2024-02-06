a45040cb02a01e210820765e1cfb9e9fa74b6390
Busola <ifebusolaj95@gmail.com>
2024-02-06 18:19:45 +0000

    compiled files for translations

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+'''
+This module is a flask application for a single route.
+'''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from typing import List
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    A configuration class to specify the languages for the app
+    and other default configurations.
+    """
+    LANGUAGES: List[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = 'en'
+    BABEL_DEFAULT_TIMEZONE: str = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    A function to return the prefered language based on the
+    request of the user.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """
+    The route to the home page of the app.
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
